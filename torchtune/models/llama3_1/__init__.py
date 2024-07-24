656bad322a9cd1d5fc010762bb3e46d149a8582d
bachvudinh <bachvudinh02@gmail.com>
2024-07-24 05:02:50 +0000

    update llama3.1_s

--- torchtune/models/llama3_1/__init__.py
+++ torchtune/models/llama3_1/__init__.py
@@ -4,11 +4,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ._component_builders import llama3_1, lora_llama3_1
+from ._component_builders import llama3_1, lora_llama3_1, llama3_1_s_8b
 
 from ._model_builders import (  # noqa
     llama3_1_70b,
     llama3_1_8b,
+    llama3_1_s_8b,
     lora_llama3_1_70b,
     lora_llama3_1_8b,
     qlora_llama3_1_70b,
@@ -17,6 +18,7 @@ from ._model_builders import (  # noqa
 
 __all__ = [
     "llama3_1",
+    "llama3_1_s_8b",
     "llama3_1_8b",
     "llama3_1_70b",
     "lora_llama3_1",
